2b33a6027971a69292c4dc51f741168897d45f57
nfnfj <60233866+nfnfj@users.noreply.github.com>
2022-09-08 17:36:07 -0300

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -29,8 +29,8 @@ using StringTools;
 
 class Main extends Sprite
 {
-	//var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	//var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
@@ -76,17 +76,17 @@ class Main extends Sprite
 
 		if (zoom == -1)
 		{
-			//var ratioX:Float = stageWidth / gameWidth;
-			var ratioY:Float = stageHeight / gameHeight;
-			zoom = (ratioY);
-			//gameWidth = Math.ceil(stageWidth / zoom);
-			gameHeight = Math.ceil(stageHeight / zoom);
+			var ratioX:Float = stageWidth / gameWidth;
+			//var ratioY:Float = stageHeight / gameHeight;
+			zoom = (ratioX);
+			gameWidth = Math.ceil(stageWidth / zoom);
+			//gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
 		SUtil.check();
 	
 		ClientPrefs.loadDefaultKeys();
-		addChild(new FlxGame(gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
